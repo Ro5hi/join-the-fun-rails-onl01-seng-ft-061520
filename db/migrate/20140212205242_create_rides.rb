25d1ea5fa9f746fb97ceb1eb45164b0976d8c67f
Shirlen Detablan <slen.detablan@gmail.com>
2020-09-03 17:43:58 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -2,6 +2,8 @@ class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
       t.timestamps null: false
+      t.integer :passenger_id
+      t.integer :
     end
   end
 end
